c045a86e9f0ea28dad13345b6ad9a39722881e8a
Eadrian Basila <basilaeadrian@gmail.com>
2022-01-29 22:57:16 +0800

    Viewsets and Routers

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Footer from './components/footer';
 import Register from './components/register';
 import Login from './components/login';
 import Logout from './components/logout';
+import Single from './components/single';
 
 
 const routing = (
@@ -17,9 +18,10 @@ const routing = (
 			<Header />
 			<Switch>
 				<Route exact path="/" component={App} />
-				<Route exact path="/register" component={Register} />
-				<Route exact path="/login" component={Login} />
-				<Route exact path="/logout" component={Logout} />
+				<Route path="/register" component={Register} />
+				<Route path="/login" component={Login} />
+				<Route path="/logout" component={Logout} />
+				<Route path="/post/:slug" component={Single} />
 			</Switch>
 			<Footer />
 		</React.StrictMode>
